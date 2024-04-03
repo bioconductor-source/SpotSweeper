aa51ad11e0405fc1a7153f5817461856f2bfff37
Michael Totty <mictott@gmail.com>
2024-04-03 12:15:30 -0500

    finalized test-modifiedZ.R

--- tests/testthat/test-modifiedZ.R
+++ tests/testthat/test-modifiedZ.R
@@ -1,6 +1,13 @@
 # run examples from modifiedZ() function documentation
-example(modifiedZ)
+data <- c(1, 2, 3, 4, 5, 100)
+z_data <- modifiedZ(data)
+
+# == Tests ==
+test_that("example data gives correct Z", {
+  expect_type(z_data, "double")
+})
 
 test_that("example data gives correct Z", {
-  expect_type(z_data, "numeric")
+  expect_equal(z_data, c(-0.758240, -0.454944, -0.151648,
+                         0.151648, 0.454944, 29.268065))
 })
