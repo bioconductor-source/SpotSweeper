52771fd59bb02835283528c7b5b4202bda62f93b
Michael Totty <mictott@gmail.com>
2024-04-08 17:22:41 -0400

    small fix to localOutliers

--- R/localOutliers.R
+++ R/localOutliers.R
@@ -81,71 +81,71 @@ localOutliers <- function(
         metric_to_use <- metric
     }
 
-    # Get a list of unique sample IDs
-    unique_sample_ids <- unique(colData(spe)[[samples]])
+  # Get a list of unique sample IDs
+  unique_sample_ids <- unique(colData(spe)[[samples]])
 
-    # Initialize list to store each spaQC dataframe
-    spaQC_list <- vector("list", length(unique_sample_ids))
+  # Initialize list to store each spaQC dataframe
+  spaQC_list <- vector("list", length(unique_sample_ids))
 
-    # Loop through each unique sample ID
-    for (sample in unique_sample_ids) {
-        # Subset the data for the current sample
-        spe_subset <- spe[, colData(spe)[[samples]] ==
-            sample]
+  # Loop through each unique sample ID
+  for (sample in unique_sample_ids) {
+    # Subset the data for the current sample
+    spe_subset <- spe[, colData(spe)[[samples]] ==
+                        sample]
 
-        # Create a list of spatial coordinates and qc features
-        spaQC <- colData(spe_subset)
+    # Create a list of spatial coordinates and qc features
+    spaQC <- colData(spe_subset)
 
-        # Find nearest neighbors
-        dnn <- BiocNeighbors::findKNN(spatialCoords(spe_subset),
-            k = n_neighbors, warn.ties = FALSE
-        )$index
+    # Find nearest neighbors
+    dnn <- BiocNeighbors::findKNN(spatialCoords(spe_subset),
+                                  k = n_neighbors, warn.ties = FALSE
+    )$index
 
-        # Initialize a matrix to store z-scores
-        mod_z_matrix <- array(NA, nrow(dnn))
+    # Initialize a matrix to store z-scores
+    mod_z_matrix <- array(NA, nrow(dnn))
 
-        # Loop through each row in the nearest neighbor index matrix
-        for (i in seq_len(nrow(dnn))) {
-            dnn.idx <- dnn[i, ]
-            neighborhood <- spaQC[c(i, dnn.idx[dnn.idx != 0]), ][[metric_to_use]]
+    # Loop through each row in the nearest neighbor index matrix
+    for (i in seq_len(nrow(dnn))) {
+      dnn.idx <- dnn[i, ]
+      neighborhood <- spaQC[c(i, dnn.idx[dnn.idx != 0]), ][[metric_to_use]]
 
-            # modified z-score
-            mod_z_matrix[i] <- spatialEco::outliers(neighborhood)[1]
-        }
-
-        # find outliers based on cutoff, store in colData
-        metric_outliers <- paste0(metric, "_outliers")
-        spaQC[metric_outliers] <- switch(direction,
-            higher = as.factor(apply(
-                mod_z_matrix,
-                1, function(x) x > cutoff
-            )),
-            lower = as.factor(apply(
-                mod_z_matrix,
-                1, function(x) x < -cutoff
-            )),
-            both = as.factor(apply(
-                mod_z_matrix,
-                1, function(x) {
-                    x > cutoff | x <
-                        -cutoff
-                }
-            ))
-        )
-
-        # add z-scores to colData
-        metric_z <- paste0(metric, "_z")
-        spaQC[metric_z] <- mod_z_matrix[]
-
-        # Store the modified spaQC dataframe in the list
-        spaQC_list[[sample]] <- spaQC
+      # modified z-score
+      mod_z_matrix[i] <- spatialEco::outliers(neighborhood)[1]
     }
 
-    # rbind the list of dataframes
-    spaQC_aggregated <- do.call(rbind, spaQC_list)
+    # find outliers based on cutoff, store in colData
+    metric_outliers <- paste0(metric, "_outliers")
+    spaQC[metric_outliers] <- switch(direction,
+                                     higher = as.factor(apply(
+                                       mod_z_matrix,
+                                       1, function(x) x > cutoff
+                                     )),
+                                     lower = as.factor(apply(
+                                       mod_z_matrix,
+                                       1, function(x) x < -cutoff
+                                     )),
+                                     both = as.factor(apply(
+                                       mod_z_matrix,
+                                       1, function(x) {
+                                         x > cutoff | x <
+                                           -cutoff
+                                       }
+                                     ))
+    )
 
-    # replace column data
-    colData(spe) <- spaQC_aggregated
+    # add z-scores to colData
+    metric_z <- paste0(metric, "_z")
+    spaQC[metric_z] <- mod_z_matrix[]
 
-    return(spe)
+    # Store the modified spaQC dataframe in the list
+    spaQC_list[[sample]] <- spaQC
+  }
+
+  # rbind the list of dataframes
+  spaQC_aggregated <- do.call(rbind, spaQC_list)
+
+  # replace column data
+  colData(spe) <- spaQC_aggregated
+
+  return(spe)
 }
